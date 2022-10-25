8a58a681f3b0bca147cf33ab1d0531b5b80c6547
shiboyang <sby089@126.com>
2022-10-25 19:21:06 +0800

    :construction: proj4

--- shenlan/Project4/libs/buff.cpp
+++ shenlan/Project4/libs/buff.cpp
@@ -5,13 +5,22 @@
 #include "buff.h"
 #include "role.h"
 
+bool Buff::expired() const {
+    return expire <= 0;
+}
 
 void LossBuff::operator()(Person *user) {
+    if (expire-- > 0) {
+        std::cout << message << ", " << user->state() << std::endl;
+        user->injure(2);
+    }
 }
 
 void CureBuff::operator()(Person *user) {
-    if (this->expire > 0) {
-        user->cure(this->hp);
-        --this->expire;
+    if (expire-- > 0)
+    {
+        user->cure(hp);
+        std::cout << message << ", " << user->state() << std::endl;
     }
 }
+
--- shenlan/Project4/libs/buff.h
+++ shenlan/Project4/libs/buff.h
@@ -7,36 +7,50 @@
 
 #include <random>
 
+enum BuffType {
+    goodBuff,
+    badBuff
+};
+
 class Person;
 
 class Buff {
 
 public:
-    explicit Buff(int ex = 1) : expire(ex) {};
+    explicit Buff(BuffType type, const int ex = 1) : expire(ex), type(type) {};
 
     virtual void operator()(Person *) = 0;
 
     virtual ~Buff() = default;
 
+    bool expired() const;
+
 protected:
     int expire;
+    BuffType type;
 };
 
 
 class LossBuff : Buff {
 public:
+    LossBuff() : Buff(badBuff) {};
+
     void operator()(Person *) override;
+
+protected:
+    const std::string message = "持续损伤效果, 失去2点生命";
 };
 
 
 class CureBuff : public Buff {
 public:
-    explicit CureBuff(unsigned int hp, int ex = 1) : Buff(ex), hp(hp) {};
+    explicit CureBuff(uint hp, const int ex = 1) : Buff(goodBuff, ex), hp(hp) {};
 
     void operator()(Person *) override;
 
 protected:
     unsigned int hp = 0;
+    const std::string message = "持续治疗效果, 恢复5点生命";
 };
 
 #endif //PROJECT4_BUFFER_H
